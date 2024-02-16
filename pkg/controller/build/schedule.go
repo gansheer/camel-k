72dbd1864fcb3bebf3c2dc4751397557e7aff805
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2024-02-16 17:15:23 +0100

    fix status update

--- pkg/controller/build/schedule.go
+++ pkg/controller/build/schedule.go
@@ -65,12 +65,27 @@ func (action *scheduleAction) Handle(ctx context.Context, build *v1.Build) (*v1.
 		return nil, err
 	} else if !allowed {
 		// Build not allowed at this state (probably max running builds limit exceeded) - let's requeue the build
-		return nil, action.toUpdatedStatus(ctx, build, schedulingCondition, build.Status.Phase)
+		// Update the condition without reseting the Build Status.
+		// This must be done in the critical section, rather than delegated to the controller.
+		return nil, action.toUpdatedCondition(ctx, build, schedulingCondition)
 	}
 
 	// Reset the Build status, and transition it to pending phase.
 	// This must be done in the critical section, rather than delegated to the controller.
 	return nil, action.toUpdatedStatus(ctx, build, schedulingCondition, v1.BuildPhasePending)
+}
+
+func (action *scheduleAction) toUpdatedCondition(ctx context.Context, build *v1.Build, condition *v1.BuildCondition) error {
+	return action.patchBuildStatus(ctx, build, func(b *v1.Build) {
+		b.Status = v1.BuildStatus{
+			Phase:      b.Status.Phase,
+			StartedAt:  b.Status.StartedAt,
+			Failure:    b.Status.Failure,
+			Conditions: b.Status.Conditions,
+		}
+		b.Status.SetConditions(*condition)
+	})
+
 }
 
 func (action *scheduleAction) toUpdatedStatus(ctx context.Context, build *v1.Build, condition *v1.BuildCondition, phase v1.BuildPhase) error {
